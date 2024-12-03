c605bc74895442b10c40284eb1ccd8ba00049ab8
Antonin <antonin.guilbaud@etu.univ-grenoble-alpes.fr>
2024-12-03 22:54:16 +0100

    test favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <ThemeProvider
           attribute="class"
