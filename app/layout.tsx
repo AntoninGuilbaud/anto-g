d0d333edd4fd2ed7352754e31ee7887246821233
Antonin <antonin.guilbaud@etu.univ-grenoble-alpes.fr>
2024-12-03 23:20:00 +0100

    metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,11 +17,22 @@ const geistMono = localFont({
 export const metadata: Metadata = {
   title: "Portfolio | Antonin Guilbaud",
   description: "Découvrir mon profil !",
-  icons:
-  {
+  icons: {
     icon: "/favicon.ico",
   },
-
+  openGraph: {
+    title: "Portfolio | Antonin Guilbaud",
+    description: "Découvrir mon profil !",
+    url: "https://anto-g.vercel.app/",
+    images: [
+      {
+        url: "/preview-image.png", // Lien vers ton image PNG
+        width: 1200,
+        height: 630,
+        alt: "Preview de mon portfolio", // Description de l'image
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -31,10 +42,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
-      <head>
-        <meta property="og:image" content="https://ogcdn.net/a6271b04-8ace-45ad-a7ef-0f945adfef8c/v1/anto-g.vercel.app/Antonin%20Guilbaud/https%3A%2F%2Fopengraph.b-cdn.net%2Fproduction%2Fimages%2F48bb9b65-b257-40d4-8ac0-9966895e70e7.jpg%3Ftoken%3Dr87pJAKCeZ6jEkT6P-qelUZ5JibLEorLxbq8rmZrWE8%26height%3D642%26width%3D642%26expires%3D33269263530/og.png
-" />
-      </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <ThemeProvider
           attribute="class"
