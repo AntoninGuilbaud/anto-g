ff40179449417024f1f040aed9a2a3aa5fee455a
Antonin <antonin.guilbaud@etu.univ-grenoble-alpes.fr>
2024-12-04 11:33:32 +0100

    "professionnelles"

--- components/Work.tsx
+++ components/Work.tsx
@@ -6,7 +6,7 @@ const Work = () => {
         <div className='py-24'>
             <h1 className="heading text-center text-3xl md:text-4xl font-bold text-gray-800 dark:text-white px-4">
                 Mes expériences
-                <span className="text-blue-300"> professionnels</span>
+                <span className="text-blue-300"> professionnelles</span>
             </h1>
             <TimelineDemo />
         </div>
